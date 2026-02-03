4dace8023d19f80d29e0eea0d028b8f48868b995
Tanvi Beri <tanvi.mberi@gmail.com>
2026-02-03 13:40:00 +0530

    Add abstract class example with Coffee

---
+++ Inheritance/Abstract.java
@@ -0,0 +1,26 @@
+package Inheritance;
+
+abstract class Coffee{
+    abstract void drink();
+        void like(){
+            System.out.println("I like coffee");
+        }
+    }
+
+
+public class Abstract{
+    public static void main(String[] args){
+        Coffee c = new Coffee(){
+            void drink(){
+                System.out.println("Coffee is a liquid.");
+            }
+             void like(){
+                System.out.println("I like coffee");
+        }
+
+    };
+    c.drink();
+    c.like();
+
+}
+}
