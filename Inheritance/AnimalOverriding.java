6aaa0a46cb131435db832c807ca7bb62cde0e834
Tanvi Beri <tanvi.mberi@gmail.com>
2026-02-09 11:30:09 +0530

    Animal Overriding update

--- Inheritance/AnimalOverriding.java
+++ Inheritance/AnimalOverriding.java
@@ -13,9 +13,18 @@ class Cat extends Animal{
     }
 }
 
+class Dog extends Animal{
+    @Override
+    void sound(){
+        System.out.println("BOO!");
+    }
+}
+
 public class AnimalOverriding{
     public static void main(String[] args){
         Animal a = new Cat();
+        Animal d = new Dog();
         a.sound();
+        d.sound();
     }
 }
