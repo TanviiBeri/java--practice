6d9f848494bc378e5325c6e6c1df89d4312fb749
Tanvi Beri <tanvi.mberi@gmail.com>
2026-02-24 13:01:49 +0530

    modified with throw

--- ExcepHandling/Example.java
+++ ExcepHandling/Example.java
@@ -7,10 +7,16 @@ public class Example {
             throw new NullPointerException("demo");
         } catch (NullPointerException e) {
             System.out.println("caught inside fun()");
+            throw e; //rethrowing the exception
         }
     }
 
     public static void main(String[] args) {
-        fun();
+        try{
+            fun();
+        }
+        catch (NullPointerException e){
+            System.out.println("caught in main.");
+        }
     }
 }
